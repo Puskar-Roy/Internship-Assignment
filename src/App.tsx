3fc56261901a397f8e5c8a9ef665b9b509db01c7
Puskar-Roy <puskarroy600@gmail.com>
2023-12-05 22:40:35 +0530

    feat : component 1

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Firstpage />} />
+        
         <Route path="/user" element={<Secondpage/>} />
       </Routes>
     </>
