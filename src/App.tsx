b1ec783ed1c073c4a428396622b918535ebd9326
Puskar-Roy <puskarroy600@gmail.com>
2023-12-05 22:42:47 +0530

    feat : component 1

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import Firstpage from './components/FirstPage/Firstpage';
 import Secondpage from './components/Secondpage/Secondpage';
 
 
+
 function App() {
 
   return (
     <>
       <Routes>
         <Route path="/" element={<Firstpage />} />
+        <Route path="/user" element={<Secondpage />} />
+
         
-        <Route path="/user" element={<Secondpage/>} />
       </Routes>
     </>
   );
